c80b376b79f081af2bf596bb24912311fe28ca92
Lauvidal <lauracatalina019@gmail.com>
2023-02-06 16:58:33 -0500

    commit de laura vidal

---
+++ src/LauraVidal/src/patronesdediseño/SecadorEncender.java
@@ -0,0 +1,15 @@
+package patronesdediseño;
+
+public class SecadorEncender implements Interfaz {
+	
+	@Override
+	public void peso() {
+		System.out.println("Encendiendo...");
+		
+	}
+	
+
+}
+ 
+
+
---
+++ src/LauraVidal/src/patronesdediseño/SecardorTemperatura2.java
@@ -0,0 +1,15 @@
+package patronesdediseño;
+
+public class SecardorTemperatura2 implements Interfaz {
+	
+	@Override
+	public void peso() {
+		System.out.println("Temperatura 2...");
+		
+	}
+	
+
+
+}
+
+
