03d441cc7b146871956dcf776df0886e6a5e3b32
Your <lopez@gmail.com>
2023-02-20 16:15:59 -0500

    commit delegate sebas alvarado

---
+++ Delegate/Delegate sebas Alvarado/Persona.java
@@ -0,0 +1,26 @@
+package patrones_de_diseño;
+
+public class Persona{
+
+public void alimentar (String nombre) {
+		
+		System.out.println(nombre+" es hora de comer");
+		
+	
+	}
+
+public void camina () {
+	
+	System.out.println("a caminar un poco");
+	
+
+}
+
+public void duerme () {
+	
+	System.out.println("durmiendoo");
+	
+
+}
+	
+}
---
+++ Delegate/Delegate sebas Alvarado/estudiante.java
@@ -0,0 +1,19 @@
+package patrones_de_diseño;
+
+public class estudiante {
+
+	
+public void ir_a_clases () {
+		
+		System.out.println("estudiando en la U");
+		
+	
+	}
+
+public void hacer_la_tarea () {
+	
+	System.out.println("haciendo los trabajos");
+	
+}
+
+}
